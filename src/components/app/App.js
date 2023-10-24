4cd21729e2face845bb964ded01c55abe91a78c8
Honor <nik26sych@gmail.com>
2023-10-24 22:45:16 +0300

    search pagination

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from "react";
 import Cards from "../card/Card.js"
 import './App.css'
-import {Spin, Input, Tabs, Alert} from  'antd';
+import {Spin, Input, Pagination, Alert} from  'antd';
 import { LoadingOutlined } from '@ant-design/icons';
-import { AndroidOutlined, AppleOutlined } from '@ant-design/icons'; 
-const apiUrl = `https://api.themoviedb.org/3/movie/11?api_key=${process.env.REACT_APP_MOVIEDB_API_KEY}`;
+import _ from 'lodash'
+const apiUrl = `https://api.themoviedb.org/3/search/movie?query=Jar&api_key=${process.env.REACT_APP_MOVIEDB_API_KEY}`;
 
 const antIcon = (
   <LoadingOutlined
@@ -20,60 +20,109 @@ const antIcon = (
 class App extends Component{
   state = {
     movies: [],
-    isLoading: true,
+    isLoading: false,
     error: false,
-  }
-  onError = ()=>{
+    searchText: "",
+    totalResults: 0,
+    currentPage:1,
+    }
+
+    searchUrl = (text) => `https://api.themoviedb.org/3/search/movie?query=${text}&api_key=${process.env.REACT_APP_MOVIEDB_API_KEY}`;
+  // componentDidMount(){
+  //   fetch(apiUrl)
+  //   .then((response)=>{
+  //     if(!response.ok){
+  //       throw new Error('Ошибка при получении данных')
+  //     }
+  //     return response.json()
+  //   })
+  //   .then((data)=>{
+  //     // fetch(https://api.themoviedb.org/3/movie/343611?&append_to_response=videos&api_key=6528ce5549843254c5037761d14d9e6e)
+  //     this.setState({movies: data, isLoading: false})
+      
+  //   })
+  //   .catch(this.onError)
+  // }
+  onError(error){
     this.setState({
       error: true,
+      errorMessage: error.message,
       isLoading: false,
       
     });
   }
   
-  componentDidMount(){
-    fetch(apiUrl)
-    .then((response)=>{
-      if(!response.ok){
-        throw new Error('Ошибка при получении данных')
-      }
-      return response.json()
-    })
-    .then((data)=>{
-      this.setState({movies: data, isLoading: false})
-      
-    })
-    .catch(this.onError)
+  handleSearch = (event) => {
+    const searchText = event.target.value;
+    this.setState({ searchText: searchText})
+
+    if (searchText){
+      this.debouncedSearch(searchText, 1)
+    }
   }
+
+  debouncedSearch = _.debounce((text, page) => {
+    this.setState({ isLoading: true, error: false });
+
+    fetch(this.searchUrl(text))
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.setState({
+          movies: data.results,
+          isLoading: false,
+          totalResults: data.total_results,
+          currentPage: page,
+        });
+      })
+      .catch(() => {
+        this.setState({ error: true, isLoading: false });
+      });
+  }, 300);  
+  handlePageChange = (page) => {
+    this.setState({ isLoading: true });
+    this.debouncedSearch(this.state.searchText, page);
+  };
   render(){
-    const {movies, isLoading, error} = this.state;
-    const spin = isLoading ? <Spin indicator={antIcon} /> : null;
-    const cardsList = !isLoading&&!error? 
-    <>
-      <Cards moviesProps={movies} />
-      <Cards moviesProps={movies} />
-      <Cards moviesProps={movies} />
-      <Cards moviesProps={movies} />
-      <Cards moviesProps={movies} />
-      <Cards moviesProps={movies} />
-    </>:null
-    const showAlert =error ?
-    <>
-    <Alert
+    const {movies, isLoading, error, searchText, totalResults, currentPage} = this.state;
+    console.log(movies.results)
+    const loader = isLoading&&<Spin indicator={antIcon} /> 
+    const errorMessage = error&&(<Alert
     message="EWH-EWH!"
     description="it's comething wrong, ya"
     type="error"
-    showIcon
-  />
-  </>:null
- 
+    showIcon/>
+    )
+    const searchResults = (
+      <>
+        {movies.map(movie => (
+          <Cards key={movie.id} movieProps={movie} />
+        ))}
+        <Pagination
+          current={currentPage}
+          total={totalResults}
+          onChange={this.handlePageChange}
+          pageSize={6}  // Зависит от API, сколько результатов на странице
+        />
+      </>
+    );
+    
 return(
       
       <div className="container">
-      <Input placeholder="Type to search..." />
-      {showAlert}
-      {spin}
-      {cardsList}
+      <Input 
+      placeholder="Type to search..."
+      value = {searchText}
+      onChange={this.handleSearch}
+      />
+      {loader}
+      {errorMessage}
+      {!isLoading && !error ? searchResults : null}
+      
       </div>
 
     )
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { Button, Card, Flex, Typography} from 'antd';
+import React, {Component} from 'react';
+import {  Card, Flex, Typography} from 'antd';
 import { format, parseISO } from 'date-fns';
 import './Card.css'
-const {Text, Link} = Typography
-function Cards(props){
-    const srcImg = `https://image.tmdb.org/t/p/w185${props.moviesProps.poster_path}`;
-    const parsedDate = parseISO(props.moviesProps.release_date);
-    const formattedDate = format(parsedDate, "MMMM d,  yyyy");
-    const cardDescription = function(text){
-        if (text.length<=160){
-            return text
-        }
-        const lastSpace = text.lastIndexOf(' ', 160);
-
-        if (lastSpace === -1) {
-            return `${text.slice(0, 160)} ...`; 
-        }
-
-        return `${text.slice(0, lastSpace)}   ...`;
+const {Text, Title} = Typography
+const apiMovieURL = `https://api.themoviedb.org/3/movie/343611?&append_to_response=videos&api_key=${process.env.REACT_APP_MOVIEDB_API_KEY}`
+class Cards extends Component{
+    render(){
+    const { poster_path, release_date, title, overview, genre_ids } = this.props.movieProps
+    const srcImg = `https://image.tmdb.org/t/p/w185${poster_path}`;
+    let formattedDate = '';
+    if (release_date){
+      const parsedDate = parseISO(release_date);
+      formattedDate = format(parsedDate, "MMMM d, yyyy")
+    }
+    
+    const cardDescription = (text) =>{
+    const shortened = text.length<=160?text:`${text.slice(0, 160)} ...`;
+    return shortened;
     }
     return(
   <Card
@@ -35,24 +34,24 @@ function Cards(props){
         vertical align="flex-start"
         className="card__info"
       >
-        <Typography.Title 
+        <Title 
         level={4}
         className='card__title'>
-          {props.moviesProps.title}
-        </Typography.Title>
+          {title}
+        </Title>
         <Text
         className='card__date'>{formattedDate}</Text>
         <div className='card__genres'>
-        {props.moviesProps.genres.map((elem)=>{
+        {genre_ids.map((genre)=>{
             return (
-            <Text code id={elem.id}>{elem.name}res</Text>
+            <Text code id={genre.id}>{genre.name}</Text>
                 )
         }
         )}
         </div>
-        <Text className='card__description'>{cardDescription(props.moviesProps.overview)}</Text>
+        <Text className='card__description'>{cardDescription(overview)}</Text>
       </Flex>
     </Flex>
   </Card>
-)};
+)}};
 export default Cards;
